a5f42be7f30bc3ca55e1e8c2a2254c2668053cee
AntFlopGame <73836816+AntFlopGame@users.noreply.github.com>
2024-08-03 22:17:52 +0300

    fix bags

--- src/components/cabinet/Contracts/Table/index.tsx
+++ src/components/cabinet/Contracts/Table/index.tsx
@@ -5,51 +5,60 @@ import { CreateGrid, CreateColumn } from "./TableComponents";
 import { useScopedI18n } from "@/locales/client";
 
 const Table = () => {
-  const theme = useTheme();
-  const isMatch = useMediaQuery(theme.breakpoints.down(1200));
-  const scopedT = useScopedI18n("Cabinet.Contracts");
-  const columns = CreateColumn();
-  const rows = CreateGrid();
+    const theme = useTheme();
+    const isMatch = useMediaQuery(theme.breakpoints.down(1200));
+    const scopedT = useScopedI18n("Cabinet.Contracts");
+    const columns = CreateColumn();
+    const rows = CreateGrid();
 
-  return (
-    <Paper
-      sx={{
-        px: 2,
-        py: 2,
-        mt: { xs: 2, md: 2, lg: 0 },
-        display: "flex",
-        flexDirection: "column",
-        height: "100%",
-
-        maxHeight: 600,
-      }}
-    >
-      <Box sx={{ pb: 2 }}>
-        <Typography sx={{ fontSize: 18, textTransform: "uppercase" }}>
-          {scopedT("analyseContracts.title2")}
-        </Typography>
-      </Box>
-      <Box sx={{ flexGrow: 1, height: 100, minHeight: 400, overflow: "auto" }}>
-        {!isMatch ? (
-          <DataGrid
+    return (
+        <Paper
             sx={{
-              height: "100%",
-              "& .MuiDataGrid-cell": { whiteSpace: "inherit" },
+                px: 2,
+                py: 2,
+                mt: { xs: 2, md: 2, lg: 0 },
+                display: "flex",
+                flexDirection: "column",
+                height: "100%",
+
+                maxHeight: 600,
             }}
-            columns={columns}
-            rows={rows}
-            slotProps={{
-              pagination: {
-                labelRowsPerPage: scopedT("contracts.table.footer"),
-              },
-            }}
-          />
-        ) : (
-          <MobileTable />
-        )}
-      </Box>
-    </Paper>
-  );
+        >
+            <Box sx={{ pb: 2 }}>
+                <Typography sx={{ fontSize: 18, textTransform: "uppercase" }}>
+                    {scopedT("analyseContracts.title2")}
+                </Typography>
+            </Box>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    height: 100,
+                    minHeight: 400,
+                    overflow: "auto",
+                }}
+            >
+                {!isMatch ? (
+                    <DataGrid
+                        sx={{
+                            height: "100%",
+                            "& .MuiDataGrid-cell": { whiteSpace: "inherit" },
+                        }}
+                        columns={columns}
+                        rows={rows}
+                        slotProps={{
+                            pagination: {
+                                labelRowsPerPage: scopedT(
+                                    "contracts.table.footer"
+                                ),
+                            },
+                        }}
+                    />
+                ) : (
+                    <MobileTable />
+                )}
+            </Box>
+        </Paper>
+    );
 };
 
 export default Table;
--- src/components/landing/ScrollHandler.ts
+++ src/components/landing/ScrollHandler.ts
@@ -3,43 +3,44 @@ import { useEffect, useCallback, useContext } from "react";
 import { HomeContext, tabsDesktop } from "@/context/HomeContext";
 
 const ScrollHandler: React.FC = () => {
-  const homeData = useContext(HomeContext);
+    const homeData = useContext(HomeContext);
 
-  const handleIntersection = useCallback(
-    (entries: IntersectionObserverEntry[]) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          const id = entry.target.id;
-          const matchedTab = tabsDesktop.find(
-            (element: any) => element.href === `#${id}`
-          );
-          if (matchedTab) {
-            homeData?.setActiveTab(matchedTab.id);
-            window.history.pushState(null, "", `#${id}`);
-          }
-        }
-      });
-    },
+    const handleIntersection = useCallback(
+        (entries: IntersectionObserverEntry[]) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    const id = entry.target.id;
+                    const matchedTab = tabsDesktop.find(
+                        (element: any) => element.href === `#${id}`
+                    );
+                    console.log(id);
+                    if (matchedTab) {
+                        homeData?.setActiveTab(matchedTab.id);
+                        window.history.pushState(null, "", `#${id}`);
+                    }
+                }
+            });
+        },
 
-    [homeData]
-  );
+        [homeData]
+    );
 
-  useEffect(() => {
-    const sections = document.querySelectorAll(".sections");
-    const options = {
-      root: null,
-      rootMargin: "0px",
-      threshold: 0.5,
-    };
+    useEffect(() => {
+        const sections = document.querySelectorAll(".sections");
+        const options = {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.5,
+        };
 
-    const observer = new IntersectionObserver(handleIntersection, options);
-    sections.forEach((section) => observer.observe(section));
-    return () => {
-      sections.forEach((section) => observer.unobserve(section));
-    };
-  }, [handleIntersection]);
+        const observer = new IntersectionObserver(handleIntersection, options);
+        sections.forEach((section) => observer.observe(section));
+        return () => {
+            sections.forEach((section) => observer.unobserve(section));
+        };
+    }, [handleIntersection]);
 
-  return null;
+    return null;
 };
 
 export default ScrollHandler;
